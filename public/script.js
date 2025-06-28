77b5b0947799dec4ed2508fe884a2ee219a3d25a
simondusek <dusek.simon@icloud.com>
2025-06-29 00:02:40 +0200

    feat: add first sample texts and non-fuctional elements. Design upgraded, glassmorphism as a major style.

---
+++ public/script.js
@@ -0,0 +1,15 @@
+
+const today = new Date();
+const date_options = { 
+    weekday: 'short',
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric' 
+};
+
+const formattedDate = today.toLocaleDateString('en-US', date_options);
+
+let h2_maindesktop = document.querySelector("#h2_maindesktop");
+
+h2_maindesktop.textContent = formattedDate;
+
